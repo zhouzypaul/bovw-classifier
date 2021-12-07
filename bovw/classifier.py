c9835008346f8822d9f1ff0fab8931d4aa65cc58
Paul Zhou <paul_zhou@brown.edu>
2021-12-07 16:30:09 -0500

    optimize code for runtime performance

--- bovw/classifier.py
+++ bovw/classifier.py
@@ -1,4 +1,5 @@
 import pickle
+import itertools
 
 import cv2
 import numpy as np
@@ -11,11 +12,18 @@ class BOVWClassifier:
     used for image classification
     args:
         num_clusters: number of clusters to use for kmeans clustering
+        num_sift_keypoints: number of SIFT keypoints to use for SIFT extraction,
+                            if None, use cv2 will auto pick a number of keypoints
     """
-    def __init__(self, num_clusters=50):
+    def __init__(self, num_clusters=50, num_sift_keypoints=None):
         self.num_clusters = num_clusters
+        self.num_keypoints = num_sift_keypoints
         self.kmeans_cluster = None
         self.svm_classifier = None
+        if num_sift_keypoints is not None:
+            self.sift_detector = cv2.SIFT_create(nfeatures=num_sift_keypoints)
+        else:
+            self.sift_detector = cv2.SIFT_create()
 
     def fit(self, X=None, Y=None):
         """
@@ -27,6 +35,9 @@ class BOVWClassifier:
             x: a list of images
             y: a list of labels (str)
         """
+        X = list(X)  # in case X, Y are numpy arrays
+        Y = list(Y)
+
         # get sift features
         sift_features = self.get_sift_features(images=X)
         # train kmeans
@@ -44,12 +55,32 @@ class BOVWClassifier:
             X: a list of images
         """
         assert self.svm_classifier is not None
+        X = list(X)  # in case X is numpy array
 
         # preprocess the images
         sift_features = self.get_sift_features(images=X)  # get sift features
         hist_features = self.histogram_from_sift(sift_features=sift_features)  # get histogram
 
         return self.svm_classifier.predict(hist_features)
+    
+    def __getstate__(self):
+        """
+        return state values that needs to be pickled
+
+        this functione is called when pickling the class during self.save(), and
+        is needed because cv2.SIFT is not picklable
+        """
+        return self.num_clusters, self.num_keypoints, self.kmeans_cluster, self.svm_classifier
+    
+    def __setstate__(self, state):
+        """
+        restore the state values after unpickling
+        """
+        self.num_clusters, self.num_keypoints, self.kmeans_cluster, self.svm_classifier = state
+        if self.num_keypoints is not None:
+            self.sift_detector = cv2.SIFT_create(nfeatures=self.num_keypoints)
+        else:
+            self.sift_detector = cv2.SIFT_create()
     
     def save(self, save_path):
         """
@@ -74,17 +105,9 @@ class BOVWClassifier:
         return:
             a list of SIFT features
         """
-        sift_features = []
-        for image in images:
-            # Convert them to grayscale
-            image =cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            # SIFT extraction
-            sift = cv2.SIFT_create()
-            kp, descriptors = sift.detectAndCompute(image, None)
-            #append the descriptors to a list of descriptors
-            sift_features.append(descriptors)  # each descriptor of shape (n_descriptors, 128)
-
-        return sift_features
+        keypoints = self.sift_detector.detect(images)
+        keypoints, descriptors = self.sift_detector.compute(images, keypoints)
+        return descriptors  # type: tuple
     
     def train_kmeans(self, sift_features):
         """
@@ -99,7 +122,10 @@ class BOVWClassifier:
         sift_features=np.concatenate(sift_features, axis=0)
 
         # train the kmeans classifier
-        self.kmeans_cluster = cluster.MiniBatchKMeans(n_clusters=self.num_clusters, random_state=0).fit(sift_features)
+        if self.kmeans_cluster is None:
+            self.kmeans_cluster = cluster.MiniBatchKMeans(n_clusters=self.num_clusters, random_state=0).fit(sift_features)
+        else:
+            self.kmeans_cluster.partial_fit(sift_features)
     
     def histogram_from_sift(self, sift_features):
         """
@@ -108,17 +134,31 @@ class BOVWClassifier:
         is used directly by the SVM for classification
         args:
             sift_features: a list of SIFT features
-        """
-        assert self.kmeans_cluster is not None, "kmeans classifier not trained"
+
+        this code is optimized for speed, but it's doing essentially the following:
 
         hist_features = []
         for sift in sift_features:
             # classification of all descriptors in the model
-            predicted_cluster = self.kmeans_cluster.predict(sift)
+            # sift.shape == (n_descriptors, 128)
+            predicted_cluster = self.kmeans_cluster.predict(sift)  # (n_descriptors,)
             # calculates the histogram
-            hist, bin_edges = np.histogram(predicted_cluster, bins=self.num_clusters)
+            # hist, bin_edges = np.histogram(predicted_cluster, bins=self.num_clusters)  # (num_clusters,)
+            hist = np.bincount(predicted_cluster, minlength=self.num_clusters)  # (num_clusters,)
             # histogram is the feature vector
             hist_features.append(hist)
-
         hist_features = np.asarray(hist_features)
+        return hist_features
+        """
+        assert self.kmeans_cluster is not None, "kmeans classifier not trained"
+
+        n_descriptors_per_image = [len(sift) for sift in sift_features]
+        idx_num_descriptors = list(itertools.accumulate(n_descriptors_per_image))
+        sift_features_of_all_images = np.concatenate(sift_features, axis=0)
+
+        predicted_cluster_of_all_images = self.kmeans_cluster.predict(sift_features_of_all_images)  # (num_examples,)
+        predicted_clusters = np.split(predicted_cluster_of_all_images, indices_or_sections=idx_num_descriptors)
+        predicted_clusters.pop()  # remove the last element, which is empty due to np.split
+        
+        hist_features = np.array([np.bincount(predicted_cluster, minlength=self.num_clusters) for predicted_cluster in predicted_clusters])
         return hist_features
